82537f3fd98b10b6563eec96b0cc54397245efb6
Christopher <christopher.carnivale@gmail.com>
2019-03-20 15:32:43 -0400

    added to server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 const { PORT } = require('./config');
 
+console.log(process.env.DATABASE_URL); 
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });

ea524b85ab16baaf3d7226f597888dc1f1f631ab
Christopher <christopher.carnivale@gmail.com>
2019-03-20 16:21:46 -0400

    removed console.log from server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ const app = require('./app');
 
 const { PORT } = require('./config');
 
-console.log(process.env.DATABASE_URL); 
-
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
